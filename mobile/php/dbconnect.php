624166f540026abeb541554be272f775a5eb4deb
Foo Roon Yi <80664992+FooRoonYi@users.noreply.github.com>
2022-07-08 13:46:40 +0800

    Update dbconnect.php

--- mobile/php/dbconnect.php
+++ mobile/php/dbconnect.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username   = "root";
-$password   = "";
-$dbname     = "mytutor_db";
+$username   = "moneymon_277088_mytutor_db";
+$password   = "573#VRG(5LKR";
+$dbname     = "moneymon_277088_mytutor_db";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
